53bdbc2d4847eb969a5f1c2194ba12931d054f62
Tom Ball <tball@microsoft.com>
2018-09-06 13:56:05 -0700

    background handlers working

--- sim/state/misc.ts
+++ sim/state/misc.ts
@@ -66,7 +66,7 @@ namespace pxsim.control {
     }
 
     export function removeHandler(handler: RefAction) {
-        // TODO
+        board().bus.remove(handler);
     }
 
     export function onEvent(id: number, evid: number, handler: RefAction) {
