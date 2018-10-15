1e7b6b720ba67e9c70cfc6ab01420f9aa6bdf9bd
Tom Ball <tballmsft@users.noreply.github.com>
2018-10-15 12:08:25 -0700

    copy over simulator code

--- sim/state/misc.ts
+++ sim/state/misc.ts
@@ -85,6 +85,13 @@ namespace pxsim.control {
     export function eventValue() {
         return board().bus.getLastEventValue()
     }
+
+    export function nextHandlerToBackground() {
+        board().bus.setBackgroundHandlerFlag();
+    }
+    export function removeFromBackground(handler: RefAction) {
+        board().bus.removeBackgroundHandler(handler);
+    }
 }
 
 namespace pxsim.pxtcore {
